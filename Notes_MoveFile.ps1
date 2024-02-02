260add21015ae3ccf348f804899c9eae4383a8b5
Mark Aberdour <mark.aberdour@leolearning.com>
2024-02-02 17:44:51 +0000

    Update Notes_MoveFile.ps1

--- Notes_MoveFile.ps1
+++ Notes_MoveFile.ps1
@@ -9,8 +9,8 @@
     [string]$FileName
 )
 
-$sourceFolder = "C:\Users\AberdourM.BG\Downloads"
-$destinationFolder = "C:\Users\AberdourM.BG\Downloads\Notes\$FolderName\$SubFolderName"
+$sourceFolder = "C:\Path\To\Downloads"
+$destinationFolder = "C:\Path\To\Downloads\Notes\$FolderName\$SubFolderName"
 
 $sourceFile = Join-Path -Path $sourceFolder -ChildPath "$FileName"
 $destinationFile = Join-Path -Path $destinationFolder -ChildPath "$FileName"
